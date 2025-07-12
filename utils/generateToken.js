12944c38aa13a2bfe86f489fa1d889c4ef426319
raviranjan mahto <rvrnjnmahto@gmail.com>
2025-07-12 11:13:17 +0530

    initial backend setup with login register, logout

---
+++ utils/generateToken.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+};
+
+module.exports = generateToken;
